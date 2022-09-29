3531c00f50ed20c8bb2606921dbc50342e6c0d13
Allan N <60264273+allannouri@users.noreply.github.com>
2022-09-29 10:22:27 +0200

    Update sql.R

--- sql.R
+++ sql.R
@@ -1,8 +1,8 @@
-MSsqlExecute <- function(SQL = NULL, DATABASE= "C4DW", closeODBC = TRUE){
+MSsqlExecute <- function(SQL = NULL, DATABASE= "XX", closeODBC = TRUE){
   # Remember to close the odbc connection
   createODBC <- function(DATABASE){
     library("RODBC")
-    conn <- odbcDriverConnect(paste0("Driver={SQL Server}; Server=db-everest-prod; Database=",
+    conn <- odbcDriverConnect(paste0("Driver={SQL Server}; Server=XX; Database=",
                                            DATABASE,
                                            "; Trusted_Connection=yes"))
   }
@@ -18,11 +18,11 @@ MSsqlExecute <- function(SQL = NULL, DATABASE= "C4DW", closeODBC = TRUE){
 }
 
 
-MSsqlSave <- function(DATA = NULL, DATABASE= "C4DW", TABLENAME = NULL, closeODBC = TRUE, OVERWRITE = FALSE){
+MSsqlSave <- function(DATA = NULL, DATABASE= "XX", TABLENAME = NULL, closeODBC = TRUE, OVERWRITE = FALSE){
   
   createODBC <- function(DATABASE){
     library("RODBC")
-    conn <- odbcDriverConnect(paste0("Driver={SQL Server}; Server=db-everest-prod; Database=",
+    conn <- odbcDriverConnect(paste0("Driver={SQL Server}; Server=XX; Database=",
                                      DATABASE,
                                      "; Trusted_Connection=yes"))
   }
@@ -76,32 +76,32 @@ MSsqlSave <- function(DATA = NULL, DATABASE= "C4DW", TABLENAME = NULL, closeODBC
 
 UpdateLog <- function(ID, SUBID, TASK, FREQ, START = TRUE, END = FALSE){
   
-  res <- MSsqlExecute(paste0("SELECT ID, SUB_ID, TASK FROM [CfResearch].[QR].[Log] WHERE TASK = '", TASK, "'"))
+  res <- MSsqlExecute(paste0("SELECT ID, SUB_ID, TASK FROM XX WHERE TASK = '", TASK, "'"))
   
   if(dim(res)[1] == 0){
-    ID <-  MSsqlExecute("SELECT 1+MAX(ID) FROM [CfResearch].[QR].[Log]")
+    ID <-  MSsqlExecute("SELECT 1+MAX(ID) FROM XX")
     SUBID <- 1
-    MSsqlExecute(paste0("INSERT INTO [CfResearch].[QR].[Log] VALUES (",paste0(ID),", ", SUBID, ", '",TASK,"', NULL, NULL,", 0, ", '", toupper(Sys.getenv("USERNAME")),"', '",FREQ,"' )"))
+    MSsqlExecute(paste0("INSERT INTO [XX VALUES (",paste0(ID),", ", SUBID, ", '",TASK,"', NULL, NULL,", 0, ", '", toupper(Sys.getenv("USERNAME")),"', '",FREQ,"' )"))
   print("Task is succesfully created")
   }
  
-  res <- MSsqlExecute(paste0("SELECT ID, SUB_ID FROM [CfResearch].[QR].[Log] WHERE ID = ", ID, " AND SUB_ID = ", SUBID))
+  res <- MSsqlExecute(paste0("SELECT ID, SUB_ID FROM XX WHERE ID = ", ID, " AND SUB_ID = ", SUBID))
   if(dim(res)[1] == 0){
-    SUBID <-  MSsqlExecute(paste0("SELECT 1+MAX(SUB_ID) FROM [CfResearch].[QR].[Log] WHERE ID = ", ID))
-    MSsqlExecute(paste0("INSERT INTO [CfResearch].[QR].[Log] VALUES (",paste0(ID),", ", SUBID, ", '",TASK,"', NULL, NULL,", 0, ", '", toupper(Sys.getenv("USERNAME")),"', '",FREQ,"' )"))
+    SUBID <-  MSsqlExecute(paste0("SELECT 1+MAX(SUB_ID) FROM XX WHERE ID = ", ID))
+    MSsqlExecute(paste0("INSERT INTO XX VALUES (",paste0(ID),", ", SUBID, ", '",TASK,"', NULL, NULL,", 0, ", '", toupper(Sys.getenv("USERNAME")),"', '",FREQ,"' )"))
   print("SUB task is succesfully created")
   }
   
   if(isTRUE(START) && isFALSE(END)){
-    MSsqlExecute(paste0("UPDATE [CfResearch].[QR].[Log] SET START_TIME = '",
+    MSsqlExecute(paste0("UPDATE XX SET START_TIME = '",
                         as.character(Sys.time()),"' , UPDATE_USER = '",toupper(Sys.getenv("USERNAME")),
                       "', END_TIME = NULL, EXEC_TIME = NULL WHERE ID = ", ID, " AND SUB_ID = ", SUBID))
   }
   
   if(isTRUE(END) && isFALSE(START)){
-    EXEC_TIME <-  MSsqlExecute(paste0("SELECT START_TIME FROM [CfResearch].[QR].[Log] WHERE ID = ", ID))
+    EXEC_TIME <-  MSsqlExecute(paste0("SELECT START_TIME FROM XX WHERE ID = ", ID))
     EXEC_TIME <- difftime(Sys.time(),EXEC_TIME$START_TIME,units = "mins")
-    MSsqlExecute(paste0("UPDATE [CfResearch].[QR].[Log] SET END_TIME = '",
+    MSsqlExecute(paste0("UPDATE XX SET END_TIME = '",
                         as.character(Sys.time()),"' , UPDATE_USER = '",toupper(Sys.getenv("USERNAME")),
                         "', EXEC_TIME = ",round(as.numeric(EXEC_TIME),4),
                         " WHERE ID = ", ID, " AND SUB_ID = ", SUBID))
@@ -116,13 +116,13 @@ AzureExecute <- function(SQL = NULL, DATABASE= NULL, closeODBC = FALSE){
   
   createODBC <- function(DATABASE){
     library("RODBC")
-    conn <- odbcDriverConnect(paste0("Driver={ODBC Driver 17 for SQL Server}; Server=mi-c4-share-register.public.c8c6f9050130.database.windows.net,3342; Database=",
+    conn <- odbcDriverConnect(paste0("Driver={ODBC Driver 17 for SQL Server}; Server=XX; Database=",
                                      DATABASE,
                                      "; Trusted_Connection=yes"))
   }
   
   if(is.null(DATABASE)){
-    DATABASE <- "cfrms_prod"
+    DATABASE <- "XX"
   }
   
   conn <- createODBC(DATABASE)
